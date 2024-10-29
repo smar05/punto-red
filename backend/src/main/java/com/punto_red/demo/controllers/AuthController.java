e56547f4201e7835f5f1eb13d2438ef7f7d339fd
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-10-29 13:41:35 -0500

    refactor: Organizacion del codigo de autenticacion

--- backend/src/main/java/com/punto_red/demo/controllers/AuthController.java
+++ backend/src/main/java/com/punto_red/demo/controllers/AuthController.java
@@ -2,32 +2,29 @@ package com.punto_red.demo.controllers;
 
 import com.punto_red.demo.models.AuthRequest;
 import com.punto_red.demo.models.AuthResponse;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
+import com.punto_red.demo.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class AuthController {
 
+    @Autowired
+    private final AuthService authService;
+
     private final String AUTH_URL = "https://us-central1-puntored-dev.cloudfunctions.net/technicalTest-developer/api/auth";
     private final String API_KEY = "mtrQF6Q11eosqyQnkMY0JGFbGqcxVg5icvfVnX1ifIyWDvwGApJ8WUM8nHVrdSkN";
 
+    public AuthController(final AuthService authService) {
+        this.authService = authService;
+    }
+
     @PostMapping("/api/auth")
     public ResponseEntity<AuthResponse> authenticate(@RequestBody final AuthRequest authRequest) {
-        final RestTemplate restTemplate = new RestTemplate();
-        final HttpHeaders headers = new HttpHeaders();
-        headers.set("x-api-key", API_KEY);
-        headers.set("Content-Type", "application/json");
-
-        final HttpEntity<AuthRequest> requestEntity = new HttpEntity<>(authRequest, headers);
-
-        final ResponseEntity<AuthResponse> response = restTemplate.exchange(AUTH_URL, HttpMethod.POST, requestEntity, AuthResponse.class);
-
-        return ResponseEntity.ok(response.getBody());
+        final AuthResponse authResponse = authService.authenticate(authRequest);
+        return ResponseEntity.ok(authResponse);
     }
 }
---
+++ backend/src/main/java/com/punto_red/demo/models/Supplier.java
@@ -0,0 +1,15 @@
+package com.punto_red.demo.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Supplier {
+    private String id;
+    private String name;
+}
