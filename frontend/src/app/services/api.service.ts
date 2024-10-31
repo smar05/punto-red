201ad32e210fa38200e5d74e7fc058ce2b59fb8b
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-10-31 11:17:25 -0500

    fix: Enviroments del front

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { IAuth } from '../interfaces/i-auth';
 import { Ipurchase } from '../interfaces/i-purchase';
 import { Isuppliers } from '../interfaces/i-suppliers';
@@ -9,7 +10,7 @@ import { Isuppliers } from '../interfaces/i-suppliers';
   providedIn: 'root',
 })
 export class ApiService {
-  private apiUrl: string = 'http://localhost:8080/api';
+  private apiUrl: string = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://puntored-latest.onrender.com/api',
+};
